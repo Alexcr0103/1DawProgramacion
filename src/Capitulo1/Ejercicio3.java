3a641e021ba171287e3e69b8a10af49c22c3e617
Alexcr0103 <alcaruco@gmail.com>
2021-10-04 14:01:01 +0200

    Ejercicio3

---
+++ src/Capitulo1/Ejercicio3.java
@@ -0,0 +1,24 @@
+package Capitulo1;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		String str = JOptionPane.showInputDialog("Introduce un número entero");
+		int var1 = Integer.parseInt(str);
+		
+		str = JOptionPane.showInputDialog("Introduce un número flotante");
+		float var2 = Float.parseFloat(str);
+		
+		str = JOptionPane.showInputDialog("Introduce un número doble");
+		double var3 = Double.parseDouble(str);
+		
+		
+		
+	
+		
+
+	}
+
+}
