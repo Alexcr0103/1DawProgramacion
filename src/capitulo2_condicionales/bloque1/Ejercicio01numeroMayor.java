d4cfdf7f403b4d4e9f68d9bbfc574007e4489c6d
Alexcr0103 <alcaruco@gmail.com>
2021-10-06 18:13:27 +0200

    Ejercicio1Bloque2

---
+++ src/capitulo2_condicionales/bloque1/Ejercicio01numeroMayor.java
@@ -0,0 +1,19 @@
+package capitulo2_condicionales.bloque1;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01numeroMayor {
+
+	public static void main(String[] args) {
+		String str = JOptionPane.showInputDialog("Introduzca un número");
+		
+		int var1 = int.parseInt (str)
+		
+		
+		if (var1 > var2)
+			System.out.println("El mayor es ");
+		
+
+	}
+
+}
