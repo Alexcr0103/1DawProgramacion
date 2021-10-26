846c1cd0477c017da6f85c837885317960cc4e2a
alexm <alexm@Aldach>
2021-10-26 20:27:12 +0200

    Bloque01Arrays

---
+++ src/capitulo04_arrays/bloque01/Ejercicio05sumaParesEimpares.java
@@ -0,0 +1,24 @@
+package capitulo04_arrays.bloque01;
+
+public class Ejercicio05sumaParesEimpares {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int array[]= new int [150];
+		int sumaPar=0, sumaImpar=0;
+		
+		for (int i=0; i<array.length;i++) {
+			array[i]= (int) Math.round(Math.random()*100);
+			
+			if (array [i] %2 ==0) {
+				sumaPar +=array[i];
+			}
+			if (array [i]%2 !=0) {
+				sumaImpar += array[i];
+			}
+		}
+		System.out.println("La suma de los números pares: " +sumaPar+ 
+				"\nLa suma de los números impares: " + sumaImpar);
+	}
+}
