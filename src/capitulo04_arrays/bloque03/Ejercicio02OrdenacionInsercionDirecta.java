f1b80c6a4982af91e3dc7a2ccf369841b80b21c5
Alexcr0103 <alcaruco@gmail.com>
2021-11-10 10:39:52 +0100

    InsercionYmetodos

--- src/capitulo04_arrays/bloque03/Ejercicio02OrdenacionInsercionDirecta.java
+++ src/capitulo04_arrays/bloque03/Ejercicio02OrdenacionInsercionDirecta.java
@@ -4,25 +4,27 @@ public class Ejercicio02OrdenacionInsercionDirecta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int array[]=new int[5];
+		int posicionMenor;
+		int array[]=new int[15];
 		
+		System.out.println("Array normal");
 		for (int i=0; i<array.length;i++) {
 			array[i]=(int) Math.round((Math.random()*1000));
 			System.out.print(array[i]+" ");
 		}
-		
-		for(int i=0;i<array.length-1;i++) {
-			if (array[i]>array[i+1]);{
-			int aux= array[i+1];
-			array[i]=array[i+1];
-			array[i+1]=aux;
+		System.out.println("\nArray invertido ");
+		for (int i = 0; i < array.length -1; i++) {
+			posicionMenor=i;
+			for (int j = i+1; j < array.length; j++) {
+				if (array[j]<array[posicionMenor])
+					posicionMenor=j;			
 			}
-			
+			int aux = array[i];
+			array[i]= array[posicionMenor];
+			array[posicionMenor]= aux;
 		}
-		System.out.println(" ");
-		for (int i=0; i<array.length;i++) {
-		System.out.print(array[i]+" ");
+		for (int i=0; i<array.length; i++) {
+			System.out.print(array[i] + " ");
 		}
 	}
 
