0a1bea62a46ccefaab9c1d4df374e5f1f9353b79
Alexcr0103 <alcaruco@gmail.com>
2021-11-11 14:56:14 +0100

    Update

--- src/MetodosAlex/MetodosYClasesAlex.java
+++ src/MetodosAlex/MetodosYClasesAlex.java
@@ -9,21 +9,37 @@ public class MetodosYClasesAlex {
 			array[i] = (int) Math.round(Math.random()*10);
 		}
 	}
-	
+	/**
+	 * 
+	 * @param array
+	 */
 	public static void inicializarArrayPor100(int array[]) {
 		
-		//array con números random de 0 a 10
+		//array con números random de 0 a 100
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) Math.round(Math.random()*100);
 		}
 	}
-	
+	/**
+	 * 
+	 * @param array
+	 */
 	public static void visualizarArray (int array[]) {
 		
 		//visualiza un array
 		for (int i = 0; i< array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
+	}
+	/**
+	 * 
+	 * @param min
+	 * @param max
+	 * @return
+	 */
+	//Obtener números aleatorios dentro de un rango.
+	public static int obtenerNumeroAzar (int min, int max) {
+		 return (int) Math.round(Math.random() * (max - min)) + min;
 	}
 	
 	
