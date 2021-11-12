46e27c40540aabe266049046f77ded0c19155688
Alexcr0103 <alcaruco@gmail.com>
2021-11-12 17:37:13 +0100

    update

--- src/MetodosAlex/MetodosYClasesAlex.java
+++ src/MetodosAlex/MetodosYClasesAlex.java
@@ -47,7 +47,7 @@ public class MetodosYClasesAlex {
 	 */
 	//Obtener números aleatorios dentro de un rango en una matriz.(Los 100 se puede cambiar por los intervalos necesarios)
 	public static void inicializarUnaMatrizAleatoriaIntervalo (int matriz [][]) {
-		for (int i = 0 ; i<matriz.length; i++) {
+		for (int i= 0 ; i<matriz.length; i++) {
 			for (int j = 0; j<matriz.length ; j ++) {
 				matriz [i][j] = (int) Math.round((Math.random()* (100-(-100)))+-100);
 			}
