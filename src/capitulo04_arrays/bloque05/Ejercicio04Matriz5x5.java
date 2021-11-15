5394f301e5d4f511888633fe2b8abadcfe50de60
alexm <alexm@Aldach>
2021-11-15 20:22:24 +0100

    Update

--- src/capitulo04_arrays/bloque05/Ejercicio04Matriz5x5.java
+++ src/capitulo04_arrays/bloque05/Ejercicio04Matriz5x5.java
@@ -7,15 +7,21 @@ public class Ejercicio04Matriz5x5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int matriz[][] = new int [5][5];
-		int matriz2[][] = new int [][] {	{1,		2,		3,		4,		 5},
+		int matriz2[][] = new int [][] {	{0,		2,		3,		4,		 0},
 											{0, 	7,		8,		8,		10},
 											{0,		0,		13,		14,		15},
-											{0,		0,		2,		19,		20},
+											{0,		0,		0,		19,		20},
 											{0,		0,		0,		0,		25}};
-											
+		
+		//MetodosYClasesAlex.inicializarUnaMatrizAleatoria(matriz);
 		esPositiva(matriz2);
 		esDiagonal(matriz2);
 		esTrianguloSuperior(matriz2);
+		esDispersa(matriz2);
+		System.out.println(" ");
+		System.out.println("La matriz unidimensional ");
+		matrizUnidimensional(matriz2);
+		
 		for (int i = 0; i < matriz.length; i ++) {
 			for (int j = 0; j < matriz.length; j++ ) {
 			}
@@ -39,8 +45,7 @@ public class Ejercicio04Matriz5x5 {
 		System.out.println(" ");
 		MetodosYClasesAlex.visualizarUnaMatriz(matriz);
 		return esPositiva;
-	}
-	
+	}	
 	public static boolean esDiagonal (int matriz[][]) {
 		boolean esDiagonal = true;
 		
@@ -66,5 +71,56 @@ public class Ejercicio04Matriz5x5 {
 		System.out.println("¿La matriz es triángulo superior? " + esTrianguloSuperior);
 		return esTrianguloSuperior;
 		
+	}
+	public static boolean esDispersa(int matriz[][]) {
+		boolean dispersa = true;
+		int count = 0;
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if (matriz[i][j] == 0) {
+					count++;
+				}
+			}
+			if (count >= 1) {
+				count = 0;
+			} else {
+				dispersa = false;
+			}
+		}
+		
+		for (int i = 0; i < matriz[0].length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if (matriz[j][i] == 0) {
+					count ++;
+				}
+			}
+			
+			if (count >= 1) {
+				count = 0;
+			} else {
+				dispersa = false;
+			}			
+		}
+		System.out.println("¿La matriz es dispersa? " + dispersa);
+		return dispersa;
+	}
+	public static int[] matrizUnidimensional(int matriz[][]) {
+		int numValores = 0;
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				numValores++;
+			}
+		}
+		
+		int array[] = new int[numValores];
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz[i].length; j++) {
+				array[i] = matriz[i][j];
+				System.out.print(array[i] + " ");
+			}
+		}
+		return array;
 	}
 }
