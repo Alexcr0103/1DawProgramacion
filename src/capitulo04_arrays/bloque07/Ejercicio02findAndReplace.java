de64806f10b34b59b523b8020617a025cf5298f0
alexm <alexm@Aldach>
2021-11-23 21:09:21 +0100

    Bloque07Ejercicios

---
+++ src/capitulo04_arrays/bloque07/Ejercicio02findAndReplace.java
@@ -0,0 +1,52 @@
+package capitulo04_arrays.bloque07;
+
+import java.util.Scanner;
+
+public class Ejercicio02findAndReplace {
+
+		public static void main(String[] args) {
+			int array[] = {1,2,3,4,5};
+			System.out.println("Array Original: ");
+			imprimirArray(array);
+			
+			System.out.println("\nIntroduce el valor buscado: ");
+			Scanner sc = new Scanner(System.in);
+			int numeroBuscado = sc.nextInt();
+			
+			System.out.println("\nIntroduce el valor sustituto: ");
+			sc = new Scanner(System.in);
+			int numeroRemplazo = sc.nextInt();
+			
+			System.out.println("\nReemplazado: ");
+			imprimirArray(findAndReplace(array, numeroBuscado, numeroRemplazo));
+			
+			
+			
+		}
+		
+		/**
+		 * 
+		 * 
+		 */
+		public static void imprimirArray (int array[]) {
+			for (int i = 0; i < array.length; i++) {
+				System.out.print (array[i] + " ");
+			}
+		}
+		
+		/**
+		 * 
+		 * 
+		 * @return 
+		 */
+		public static int[] findAndReplace (int array[], int numeroBuscado, int numeroRemplazo) {
+			
+			for (int i = 0; i < array.length; i++) {
+				if (numeroBuscado == array[i]) {
+					array[i] = numeroRemplazo;
+				}
+			}
+			 return array;
+	}
+
+}
---
+++ src/capitulo04_arrays/bloque07/Ejercicio03addInteger.java
@@ -0,0 +1,49 @@
+package capitulo04_arrays.bloque07;
+
+import java.util.Scanner;
+
+public class Ejercicio03addInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[] = {1,2,3,4,5};
+		imprimirArray(array);
+		
+		System.out.println("\nIntroduce el valor que deseas añadir: ");
+		Scanner sc = new Scanner(System.in);
+		int numAñadido = sc.nextInt();
+		
+		int array2[] = addInteger(array, numAñadido);		
+		System.out.println("Array ampliado: ");
+		imprimirArray(array2);
+		
+	}
+	
+	/**
+	 * 
+	 */	
+	public static void imprimirArray (int array2[]) {
+		for (int i = 0; i < array2.length; i++) {
+			System.out.print (array2[i] + " ");
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public static int[] addInteger (int array[], int numAñadido) {
+		
+		int array2[] = new int [array.length + 1];
+		
+		for (int i = 0; i < array2.length; i++) {
+			if (i == array2.length - 1) {
+				array2[i] = numAñadido;
+			}
+			else {
+				array2[i] = array[i];
+			}
+		}		
+		return array2; 
+	}
+
+}
